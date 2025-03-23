71e5469278e7e3b6ad86f8523b2a4ab969cf9fe2
habibapanna <habibapanna49@gmail.com>
2025-03-23 06:17:05 +0600

    again router updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
-import router from './Routes/Router';
+import router from './routes/Router';
+
+
 
 
 createRoot(document.getElementById('root')).render(
